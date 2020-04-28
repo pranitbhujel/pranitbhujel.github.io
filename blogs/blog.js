126933c0b0472f241eca01204374f36dfdafa311
pranit_bhujel <pranitbhujel47@gmail.com>
2020-04-28 22:36:22 +0530

    Navbar updated

--- blogs/blog.js
+++ blogs/blog.js
@@ -1,3 +1,3 @@
 var blogs = {"Another blog":{"title": "Another blog", "date": "2020-04-25", "description": "For measuring errors", "tags": "Let's see", "link": "./another-blog/index.html"},
-"My first blog":{"title": "My first blog", "date": "2020-04-25", "description": "Trying out whether the automated script works, because if that does I've made a masterpiece for myself. It's gonna be awesome", "tags": "Blog", "link": "./my-first-blog/index.html"},
+"My first blog":{"title": "My First blog", "date": "2020-04-25", "description": "Trying out whether the automated script works, because if that does I've made a masterpiece for myself. It's gonna be awesome", "tags": "Blog", "link": "./my-first-blog/index.html"},
 }
