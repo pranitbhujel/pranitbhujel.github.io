b50c9aa9591a1d9a87af25fde49e869cef006dc3
pranit_bhujel <pranitbhujel47@gmail.com>
2020-04-30 22:52:00 +0530

    Deleted stuff

--- blogs/blog.js
+++ blogs/blog.js
@@ -1,4 +1,3 @@
-var blogs = {
-"Functional Dependency":{"title": "Functional Dependency", "date": "2020-04-29", "description": "Man", "tags": "Functions", "link": "./functional-dependency/index.html"},
+var blogs = {"Machine Learning with Scikit-Learn":{"title": "Machine Learning with Scikit-Learn", "date": "2020-04-30", "description": "Basic step by step introduction to various Machine Learning concepts, and implementing a Supervised Learning using Scikit Learn. This can also be used as a hands-on training for learning the basic concepts behind the modeling mechanism of Scikit Learn.", "tags": "Scikit Learn, IRIS Dataset, Machine Learning", "link": "./machine-learning-with-scikit-learn/index.html"},
 "An approach to Data Science Problems":{"title": "An approach to Data Science Problems", "date": "2020-04-28", "description": "The use of modern tools and techniques that help us to quantify the data and find insights in them", "tags": "Data Science Workflow", "link": "./an-approach-to-data-science-problems/index.html"}
 }
