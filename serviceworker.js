be2fc5b02e718ca98945a3c1d2fcb637e1ab2317
pranit_bhujel <pranitbhujel47@gmail.com>
2021-04-21 16:26:35 +0530

    Service Worker Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,8 @@
-// This is a service worker that works in offline mode to handle the application.
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('/').then(function (cache) {
             return cache.addAll([
                 'index.html',
-                '/blog/',
-                '/project/',
             ]);
         })
     );
