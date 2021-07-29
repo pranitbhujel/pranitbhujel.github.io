97e0b97a9f249f9824f8fb56bf702897b1ebf946
pranit_bhujel <pranitbhujel47@gmail.com>
2021-07-30 01:21:26 +0530

    Experience Update

--- serviceworker.js
+++ serviceworker.js
@@ -1,26 +1,25 @@
-// self.addEventListener('install', function (e) {
-//     e.waitUntil(
-//         caches.open('/').then(function (cache) {
-//             return cache.addAll([
-//                 'index.html',
-//                 'blog/',
-//                 'css/',
-//                 'images/',
-//                 'js/',
-//                 'lib/',
-//                 'blog/python-redefined--all-the-basics-that-you-need-to-know',
-//                 'blog/index.html',
-//                 'images/app_icon.png'
-//             ]);
-//         })
-//     );
-// });
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('/').then(function (cache) {
+            return cache.addAll([
+                'index.html',
+                'blog/',
+                'css/',
+                'images/',
+                'js/',
+                'lib/',
+                'blog/index.html',
+                'images/app_icon.png'
+            ]);
+        })
+    );
+});
 
-// self.addEventListener('fetch', function (e) {
-//     fetch(e.request);
-//     e.respondWith(
-//         caches.match(e.request).then(function (response) {
-//             return response || fetch(e.request);
-//         })
-//     );
-// });
+self.addEventListener('fetch', function (e) {
+    fetch(e.request);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
